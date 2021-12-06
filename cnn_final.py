532e9f34df96959cec8391ae146369aa1f9f515f
JaeMin An <veryneuron1@gmail.com>
2021-12-06 18:23:27 +0900

    modify layer to 784-[32C3]-[32C3-P2]-[64C3]-[64C3-P2]-128-26

--- cnn_final.py
+++ cnn_final.py
@@ -36,18 +36,26 @@ class CNN(nn.Module):
         super(CNN, self).__init__()
         self.keep_prob = 0.6
         self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=5, stride=1, padding=1, bias=False),
+            nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU())
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=1, bias=False),
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU())
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
-        self.fc1 = nn.Linear(5 * 5 * 64, 128, bias=False)
+        self.fc1 = nn.Linear(7 * 7 * 64, 128, bias=False)
         nn.init.xavier_uniform_(self.fc1.weight)
-        self.layer3 = nn.Sequential(
+        self.layer5 = nn.Sequential(
             self.fc1,
             nn.BatchNorm1d(128),
             nn.ReLU(),
@@ -58,8 +66,10 @@ class CNN(nn.Module):
     def forward(self, x):
         out = self.layer1(x)
         out = self.layer2(out)
-        out = out.view(out.size(0), -1)
         out = self.layer3(out)
+        out = self.layer4(out)
+        out = out.view(out.size(0), -1)
+        out = self.layer5(out)
         out = self.fc2(out)
 
         return out
